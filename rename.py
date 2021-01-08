26d5612e431d989a0dd06a32fa5e402749008b74
CodingMan <CodingMan@uptt.cc>
2021-01-08 15:03:09 +0800

    Remove small image

--- rename.py
+++ rename.py
@@ -1,26 +1,55 @@
 import os
+import json
 
-path = 'D:/Git/5f/data/old_night/'
-f = []
-for (dirpath, dirnames, filenames) in os.walk(path):
-    f.extend(filenames)
-    break
+def get_file_name(path):
+    result = os.path.splitext(path)[0]
+    result = result[result.rfind('/') + 1:]
+    return result
 
-f = [f'{path}{x}' for x in f]
-print(f)
 
-i = 0
-for v in f:
-    name = v.lower()
-    if name.endswith('jpeg') or name.endswith('jpg') or name.endswith('png'):
-        while True:
-            try:
-                os.rename(v, f'{path}{i}.jpg')
-                break
-            except FileExistsError:
-                i += 1
-        i += 1
-    elif v.endswith('gif'):
-        os.remove(v)
-    else:
-        print('unknown image', v)
+def is_valid_image(name):
+    name = name.lower()
+
+    return name.endswith('jpeg') or name.endswith('jpg') or name.endswith('png')
+
+
+def get_files(path):
+    f_total = []
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        f_total.extend(filenames)
+        break
+
+    f_total = [f'{path}{x}' for x in f_total if is_valid_image(x)]
+    # print(f_total)
+    return f_total
+
+
+folder = 'old_hi'
+path = f'D:/Git/5f/data/{folder}/'
+
+f_total = get_files(path)
+for f in f_total:
+    if not is_valid_image(f):
+        print('remove', f)
+        os.remove(f)
+
+f_total = get_files(path)
+for i, f in enumerate(f_total):
+    # print('rename', f'{path}qqtemp_{i}.jpg')
+    os.rename(f, f'{path}qqtemp_{i}.jpg')
+
+max_index = 0
+f_total = get_files(path)
+for i, f in enumerate(f_total):
+    # print('rename', f'{path}qqtemp_{i}.jpg')
+    os.rename(f, f'{path}{i}.jpg')
+    max_index = i
+
+with open('index.json', encoding='utf-8') as f:
+    index_record = json.load(f)
+
+index_record[folder] = max_index
+print(index_record)
+
+with open('index.json', 'w', encoding='utf-8',) as f:
+    json.dump(index_record, f, indent=4, ensure_ascii=False)
